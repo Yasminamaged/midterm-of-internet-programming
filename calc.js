4f909b47d4169fcb91c6f5f9697ca71aebb8a486
yasmina maged <82272238+Yasminamaged@users.noreply.github.com>
2023-05-02 16:26:10 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,21 @@
+function add()
+{
+  var numOne, numTwo, sum;
+  numOne = parseInt(document.getElementById("first").value);
+  numTwo = parseInt(document.getElementById("second").value);
+  sum = numOne + numTwo;
+  document.getElementById("answer").value = sum;
+}
+function sub()
+{
+  var numOne, numTwo, sum;
+  numOne = parseInt(document.getElementById("first").value);
+  numTwo = parseInt(document.getElementById("second").value);
+  sum = numOne - numTwo;
+  document.getElementById("answer").value = sum;
+}
+function clear(){  
+  document.getElementById("first").value = "";
+  document.getElementById("second").value = "";
+  document.getElementById("sum").value = "" 
+ }  
